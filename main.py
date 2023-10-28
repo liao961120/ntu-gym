571f97970e91411a4368e829d8ae2f4e628e2078
Yongfu <liao961120@gmail.com>
2023-10-28 11:35:01 +0800

    adapt crawler to new website

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-import json
 import http.client
 from time import strftime
 from datetime import datetime
+from bs4 import BeautifulSoup
 
 def main():
     write_to_file()
 
 
 def get_curr_status():
-    # Request data from https://ntusportscenter.ntu.edu.tw/counter.txt
-    connection = http.client.HTTPSConnection('ntusportscenter.ntu.edu.tw')
-    connection.request("GET", "/counter.txt")
+    # Request data from https://rent.pe.ntu.edu.tw
+    connection = http.client.HTTPSConnection('rent.pe.ntu.edu.tw')
+    connection.request("GET", "/")
     response = connection.getresponse()
-    COUNT = json.loads(response.read().decode())
-    gym, swim = COUNT['CounterData'][0]['innerCount'].split(";")
+    r = response.read().decode()
+
+    # parse page
+    soup = BeautifulSoup(r, 'html5lib')
+    gym = soup.select_one('div.CMFlowPeople > div > div.CMCList > div:nth-child(1) > div.IC > div:nth-child(1) > span').text
+    swim = soup.select_one('div.CMFlowPeople > div > div.CMCList > div:nth-child(2) > div.IC > div:nth-child(1) > span').text
     now = datetime.now().strftime("%Y-%m-%d\t%a\t%H:%M")
     return f"{now}\t{gym}\t{swim}\n"
 
